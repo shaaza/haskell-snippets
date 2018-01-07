a0aa6ffeae194d43df425406e44aa0a0f39c0040
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2018-01-07 17:02:17 +0530

    Binary Search Tree: Add search

--- binary-search-tree/bst.hs
+++ binary-search-tree/bst.hs
@@ -3,11 +3,17 @@ module BinarySearchTree where
 
 data Tree a = Nil | Node (Tree a) a (Tree a) deriving Show
 
--- search :: Tree -> Int -> Bool
 insert :: Tree Int -> Int -> Tree Int
-
 insert Nil number = Node Nil number Nil
 insert (Node lt val rt) number
   | number == val = Node lt val lt
   | number < val = Node (insert lt number) val rt
   | number > val = Node lt val (insert rt number)
+
+-- search returns a boolean indicating whether the number exists in the tree.
+search :: Tree Int -> Int -> Bool
+search Nil number = False
+search (Node lt val rt) number
+  | number == val = True
+  | number < val = search lt number
+  | number > val = search rt number
