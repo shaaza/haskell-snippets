88eb065e47798f312e1fcf55a05a3377e8577433
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2018-01-07 17:22:57 +0530

    Add depth fn for BST

--- binary-search-tree/bst.hs
+++ binary-search-tree/bst.hs
@@ -10,14 +10,19 @@ insert (Node lt val rt) number
   | number < val = Node (insert lt number) val rt
   | number > val = Node lt val (insert rt number)
 
--- search returns a boolean indicating whether the number exists in the tree.
+-- search returns a boolean indicating whether the number exists in the BST.
 search :: Tree Int -> Int -> Bool
-search Nil number = False
+search Nil _ = False
 search (Node lt val rt) number
   | number == val = True
   | number < val = search lt number
   | number > val = search rt number
 
--- creates a new Btree from a list of integers
+-- creates a new BST from a list of integers
 bstFromList :: [Int] -> Tree Int
 bstFromList lst = foldl insert Nil lst
+
+-- depth of the BST
+depth :: Tree Int -> Int
+depth Nil = 0
+depth (Node lt val rt) = 1 + max (depth lt) (depth rt)
