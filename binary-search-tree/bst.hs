bba670089000593f9fcf1f821a31e983c363ae5f
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2018-01-07 17:14:32 +0530

    Make signatures consistent and fold left to create new bst

--- binary-search-tree/bst.hs
+++ binary-search-tree/bst.hs
@@ -1,14 +1,14 @@
 module BinarySearchTree where
--- A self-balancing integer binary search tree that doesn't allow duplicates.
+-- A self-balancing integer binary search tree that doesn't allow duplicates (this only unique node values).
 
 data Tree a = Nil | Node (Tree a) a (Tree a) deriving Show
 
-insert :: Int -> Tree Int -> Tree Int
-insert number Nil = Node Nil number Nil
-insert number (Node lt val rt)
+insert :: Tree Int -> Int -> Tree Int
+insert Nil number = Node Nil number Nil
+insert (Node lt val rt) number
   | number == val = Node lt val lt
-  | number < val = Node (insert number lt) val rt
-  | number > val = Node lt val (insert number rt)
+  | number < val = Node (insert lt number) val rt
+  | number > val = Node lt val (insert rt number)
 
 -- search returns a boolean indicating whether the number exists in the tree.
 search :: Tree Int -> Int -> Bool
@@ -18,6 +18,6 @@ search (Node lt val rt) number
   | number < val = search lt number
   | number > val = search rt number
 
--- creates a new Btree from a list/Foldable
+-- creates a new Btree from a list of integers
 bstFromList :: [Int] -> Tree Int
-bstFromList lst = foldr insert Nil lst
+bstFromList lst = foldl insert Nil lst
